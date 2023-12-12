d0ceb4ebb51c821a517b77771002fde78e76e91e
Mortalium <152898932+Mortalium@users.noreply.github.com>
2023-12-12 23:27:10 +0100

    Create Appl_Sterne.py

---
+++ Appl_Sterne.py
@@ -0,0 +1,22 @@
+def Sterne(n):
+    if(n==0):
+        print()
+    else:
+        print("*",end='')
+        Sterne(n-1)
+
+
+def Zahl(n):
+    if(n<0):
+        print("Bitte wähle eine nichtnegative ganze Zahl, die nicht erst ausgerechnet werden muss!!!")
+        n=input("Wie viele Sterne soll ich drucken?: ")
+        Zahl(int(n))
+    else:
+        print()
+        Sterne(int(n))
+        print()
+        print("Ich habe "+str(n)+" Sterne gedruckt")
+
+n=input("Wie viele Sterne soll ich drucken?: ")
+
+Zahl(int(n))
