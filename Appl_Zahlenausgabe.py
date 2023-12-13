a28056a1f771f581226887fa1daa4a2f5093f6a0
Mortalium <152898932+Mortalium@users.noreply.github.com>
2023-12-14 00:06:52 +0100

    Create Appl_Zahlenausgabe

---
+++ Appl_Zahlenausgabe.py
@@ -0,0 +1,19 @@
+def GibZahlausvon(n):
+    if(n>0):
+        print(n)
+        GibZahlausvon(n-1)
+
+def Eingabe():  
+    Zahl=input("Ab welcher Zahl (nur positive Zahlen) soll abwärts gezählt werden?: ")
+    try:
+        Zahl=int(Zahl)
+        if(Zahl>0):
+            GibZahlausvon(Zahl)
+        else:
+            print("Bitte gib einen positiven Integerwert an (ohne, dass der Computer diesen erste errechnen muss)")
+        Eingabe()
+    except:
+        print("Bitte gib einen positiven Integerwert an (ohne, dass der Computer diesen erste errechnen muss)")
+        Eingabe()
+    
+Eingabe()
