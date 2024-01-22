ae0249b836fe172cea3ba7a37e5867145a8c0d45
Mortalium <152898932+Mortalium@users.noreply.github.com>
2024-01-22 23:39:15 +0100

    Get Average from a List

---
+++ Appl_Mittelwert.py
@@ -0,0 +1,20 @@
+import Appl_Bubble_Sort
+
+def Count_Value(List,Count):
+    if Count>0:
+        amount=List[Count]+Count_Value(List,Count-1)
+        return amount
+    else:
+        return List[0]
+
+n=int(input("Wie lange soll die Liste sein: "))
+
+List=[]
+
+List=Appl_Bubble_Sort.CreateList(List,n)
+
+amount=Count_Value(List,n-1)
+
+Mittelwert=float(amount/n)
+
+print("Der Mittelwert der Liste ist: "+str(Mittelwert))
