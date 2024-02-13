de775e8c5112ed25a86034c74fc5fec275ae4f6f
Mortalium <152898932+Mortalium@users.noreply.github.com>
2024-02-13 23:11:03 +0100

    Redid some things in Imp_Bubble_Sort for better testing

--- Imp_Bubble_Sort.py
+++ Imp_Bubble_Sort.py
@@ -10,26 +10,34 @@ def CreateList():
 
     match mode:
         case "man":
-            for i in range(n):
-                Zahl=int(input("Zahl "+i+": "))
-                List.append(Zahl)
+            List=CreateManList(List,n)
         case "rand":
             Limit=int(input("Limit: "))
-            for i in range(n):
-                Zahl=random.randint(0,Limit)
-                List.append(Zahl)
+            print()
+            List=CreateRandList(List,n,Limit)
+    return List
+
+def CreateManList(List,n):
+    for i in range(n):
+        Zahl=int(input("Zahl "+i+": "))
+        List.append(Zahl)
+    return List
+
+def CreateRandList(List,n,Limit):
+    for i in range(n):
+        Zahl=random.randint(0,Limit)
+        List.append(Zahl)
     return List
 
 def BBSort(List):
     change=True
-
     while change:
         change=False
-        for i in range(len(List)):
-            if(List[i]>List[i+1]):
-                tmp=List[i]
-                List[i]=List[i+1]
-                List[i+1]=tmp
+        for i in range(1,len(List)):
+            if(List[i-1]>List[i]):
+                tmp=List[i-1]
+                List[i-1]=List[i]
+                List[i]=tmp
                 change=True
     return List
 
